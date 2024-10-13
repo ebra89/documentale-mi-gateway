40675420fba906e07455912c457c1bd1bbce62ab
Ebrahim <ebra.fazeli@gmail.com>
2024-10-13 18:27:44 +0200

    fix gateway route

--- api-gateway/src/main/java/it/example/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/it/example/gateway/config/SecurityConfig.java
@@ -3,42 +3,32 @@ package it.example.gateway.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-/*import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;*/
-/*import org.springframework.security.config.Customizer;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;*/
-
 @Configuration
-//@EnableWebFluxSecurity
-//@EnableWebFluxSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Autowired
     private ApplicationProperties appProps;
 
-/*    @Bean
+    @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
         serverHttpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(authorizeExchangeSpec -> {
-                    authorizeExchangeSpec.anyExchange().permitAll();
-                });
-                *//*.authorizeExchange(exchange-> exchange
+                .authorizeExchange(exchange-> exchange
                         .pathMatchers("/eureka/**")
                         .permitAll()
                         .anyExchange()
                         .authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));*//*
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return serverHttpSecurity.build();
 
-    }*/
+    }
 
 /*    @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
